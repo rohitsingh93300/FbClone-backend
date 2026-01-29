c1d27abf8949e0cdb4a5116e1025c95602957254
rohitsingh93300 <rohitsingh93300@gmail.com>
2026-01-29 13:15:52 +0530

    origin fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:['http://localhost:5173', 'https://fb-clone-frontend-nine.vercel.app'],
+    origin:'https://fb-clone-frontend-nine.vercel.app',
     credentials:true
 }))
 
